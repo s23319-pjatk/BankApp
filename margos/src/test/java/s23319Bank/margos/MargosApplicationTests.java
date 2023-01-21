c72b0512df5d263fab1c081e7d74164ba0f06c7d
s23319-pjatk <73029837+s23319-pjatk@users.noreply.github.com>
2023-01-21 12:15:16 +0100

    Add files via upload

---
+++ margos/src/test/java/s23319Bank/margos/MargosApplicationTests.java
@@ -0,0 +1,13 @@
+package s23319Bank.margos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MargosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
